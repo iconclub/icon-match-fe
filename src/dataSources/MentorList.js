d47e33befa5ca6e4baa599ab019a7d30db50f560
hiepnguyen6014 <hiepnguyen6014@gmail.com>
2022-11-05 14:47:51 +0700

    chore: setup 1

---
+++ src/dataSources/MentorList.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    mentorId: "52101007",
+    name: "Huỳnh Quốc Trường",
+    nickname: "Kunzz",
+    desc: "Tên: Huỳnh Quốc Trường Ngành học : Mạng máy tính và truyền thông dữ liệu Các công nghệ đang dùng: đang tìm hiểu cơ bản về reactjs Có thể hỗ trợ: phương pháp lập trình, lí thuyết cơ bản về ngôn ngữ C Định hướng tương lai: lập trình wed và ứng dụng Sở thích: học ra học chơi ra chơi",
+  },
+  {
+    mentorId: "519K0013",
+    name: "Nguyễn Đình Minh Khôi",
+    nickname: "Khôi Chúa Hề 🤡",
+    desc: "Chuyên về front end và một ít giải thuật mô hình, hướng nội nhưng social hơi kinh, đẹp trai và việc học phải gọi là đỉnh, cái gì cũng biết mỗi tội không biết cách biến em làm của riêng mình 🤡.",
+  },
+];
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
+// libs
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import "./index.css";
+// components
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
