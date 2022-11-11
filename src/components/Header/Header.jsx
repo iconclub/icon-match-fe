e8c2a0c494266a3e670a85afc0c8b1f0276f5ca0
iamphduc <iamphduc@gmail.com>
2022-11-11 17:20:57 +0700

    fix: logo redirect

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,12 @@ import styles from "./Header.module.scss";
 import logoImg from "~/assets/logo.png";
 
 const Header = () => {
+  const homeUrl = import.meta.env.PROD ? "/icon-match-fe" : "/";
+
   return (
     <div className={styles["header"]}>
       <div className={styles["header__inner"]}>
-        <a href="/">
+        <a href={homeUrl}>
           <img className={styles["header__logo"]} src={logoImg} alt="logo" />
         </a>
         <h1 className={styles["header__name"]}>Mentor Match</h1>
